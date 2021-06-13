bbdf94a1a557c74d48c8d2ca9c66ba5d20658547
artborodin <artborodin@gmail.com>
2021-06-13 23:21:11 +0300

    Update game.go

--- game.go
+++ game.go
@@ -1,1 +1,62 @@
 
+package main
+
+import "fmt"
+
+func main() {
+	words := []string{"тест", "привет", "мир"}
+
+	//LOOP:
+	for _, word := range words {
+
+		letters := []rune(word)      // буквы слова
+		guess_map := map[rune]bool{} // карта открытых букв
+
+		fmt.Println("Новое слово, букв: ", len(letters))
+		for {
+			var input string
+
+			fmt.Print("Введите 1 букву ")
+			_, err := fmt.Scanf("%s", &input)
+
+			if err != nil {
+				fmt.Println("error input", err)
+				continue
+			}
+			if input == "END" {
+				fmt.Println()
+				break
+			}
+
+			// д.б. введен один символ
+			if len([]rune(input)) != 1 {
+				//fmt.Pr
+				continue
+			}
+			// введенная буква
+			guess_letter := ([]rune(input))[0]
+
+			for _, ltr := range letters {
+				if guess_letter == ltr {
+					guess_map[ltr] = true
+					//fmt.Println("Ok!")
+				}
+			}
+			// формирование строки с открытыми буквами
+			var progress []rune
+			for _, ltr := range letters {
+				if guess_map[ltr] {
+					progress = append(progress, ltr)
+				} else {
+					progress = append(progress, '*')
+				}
+			}
+			fmt.Println(string(progress))
+
+			if string(progress) == word {
+				//fmt.Println("слово отгадано!")
+				break
+			}
+		}
+	}
+}
